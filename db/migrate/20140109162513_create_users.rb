7b41d81d925808dfb7a61bd3dcb58e6abfcf77e5
Tyler Wasden <twasden@gmail.com>
2014-01-09 10:50:11 -0600

    updated routes and db field:

--- db/migrate/20140109162513_create_users.rb
+++ db/migrate/20140109162513_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.fixnum :age
+      t.integer :age
 
       t.timestamps
     end
